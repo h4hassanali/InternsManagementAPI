0a76b10807515d5b54cd40bfbe4bb2e7f2a5ec6d
h4hassanali <hxn112233@gmail.com>
2024-10-22 16:35:52 +0500

    Code Pushed

---
+++ main.py
@@ -0,0 +1,19 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from intern.routes import intern_router
+from database.client import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    client = await init_db()
+    try:
+        yield
+    finally:
+        client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+app.include_router(intern_router, prefix="/intern")
